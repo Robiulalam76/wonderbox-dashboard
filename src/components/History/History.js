0b8404cff73cbbb97ed05c9536e31beced6414b9
Robiulalam76 <robiulalam762400@gmail.com>
2023-06-14 20:54:34 +0600

    hsitory add with pagination from server

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,8 +1,43 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import { Breadcrumb, Card, Container } from "reactstrap";
-
 function History() {
+  const [histories, setHistories] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const fetchHistories = async () => {
+    try {
+      fetch(`http://localhost:5000/api/history?page=${page}`)
+        .then((res) => res.json())
+        .then((data) => {
+          const { histories, totalPages, totalCount } = data;
+          setHistories(histories);
+          setTotalPages(totalPages);
+          setTotalCount(totalCount);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchHistories();
+  }, [page]);
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
+  };
   return (
     <Fragment>
       <Breadcrumb title="Hisoty" parent="History" />
@@ -12,32 +47,63 @@ function History() {
             <thead className="bg-info">
               <tr>
                 <th>#</th>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <th key={index}>Table heading</th>
-                ))}
+                <th>Title</th>
+                <th>Message</th>
+                <th>Type</th>
+                <th>Date</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <td key={index}>Table cell {index}</td>
+              {histories?.length > 0 &&
+                histories.map((history, i) => (
+                  <tr key={i}>
+                    <td style={{ maxWidth: "10px" }}>{i + 1}</td>
+                    <td style={{ maxWidth: "200px" }}>{history?.title}</td>
+                    <td style={{ maxWidth: "400px" }}>{history?.message}</td>
+                    <td>
+                      <span
+                        className={`badge rounded-pill
+                        ${
+                          (history?.type === "product" && "text-bg-primary") ||
+                          (history?.type === "wishlist" && "text-bg-info") ||
+                          (history?.type === "order" && "text-bg-success") ||
+                          (history?.type === "user" && "text-bg-info") ||
+                          (history?.type === "store" && "text-bg-light") ||
+                          (history?.type === "review" && "text-bg-secondary") ||
+                          (history?.type === "add_cart" && "text-bg-info")
+                        }`}
+                      >
+                        {history?.type}
+                      </span>
+                    </td>
+                    <td>
+                      {new Date(
+                        history.createAt && history.createAt
+                      ).toLocaleDateString("en-GB")}
+                    </td>
+                  </tr>
                 ))}
-              </tr>
-              <tr>
-                <td>2</td>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <td key={index}>Table cell {index}</td>
-                ))}
-              </tr>
-              <tr>
-                <td>3</td>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <td key={index}>Table cell {index}</td>
-                ))}
-              </tr>
             </tbody>
           </Table>
+          <div className="d-flex justify-content-end justify-items-center p-4">
+            <Button
+              variant="primary"
+              onClick={handlePreviousPage}
+              disabled={page === 1}
+            >
+              Previous
+            </Button>
+            <span style={{ margin: "0 10px" }}>
+              Page {page} of {totalPages}
+            </span>
+            <Button
+              variant="primary"
+              onClick={handleNextPage}
+              disabled={page === totalPages}
+            >
+              Next
+            </Button>
+          </div>
         </Card>
       </Container>
     </Fragment>
