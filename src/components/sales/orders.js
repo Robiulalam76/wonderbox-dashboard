8638dd939d6d20f7a093b611b329bd8427b08d79
Robiulalam76 <robiulalam762400@gmail.com>
2023-06-21 15:04:42 +0600

    orders page udated

--- src/components/sales/orders.js
+++ src/components/sales/orders.js
@@ -2,19 +2,27 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Breadcrumb from "../common/breadcrumb";
 import { Card, CardBody, CardHeader, Container } from "reactstrap";
-import { Button, Table } from "react-bootstrap";
+import { Button, Dropdown, Table } from "react-bootstrap";
 import { useContext } from "react";
 import { AuthContext } from "../../ContextAPI/AuthProvider";
+import moment from "moment";
 
 const Orders = () => {
   const { stores } = useContext(AuthContext);
   const [orders, setOrders] = useState([]);
   const [storeId, setStoreId] = useState("");
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
   const handleGetOrders = (id) => {
-    fetch(`http://localhost:5000/api/card/orders/${id}`)
+    fetch(`http://localhost:5000/api/card/orders/${id}?page=${currentPage}`)
       .then((res) => res.json())
-      .then((data) => setOrders(data));
+      .then((data) => {
+        setOrders(data.data);
+        setCurrentPage(data.page);
+        setTotalPages(data.totalPages);
+      });
   };
 
   const handleSetStoreId = (id) => {
@@ -24,7 +32,37 @@ const Orders = () => {
 
   useEffect(() => {
     handleGetOrders(stores[0]?._id);
-  }, [stores]);
+  }, [stores, currentPage]);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  console.log(orders);
+
+  const handleUpdateStatus = (id, status) => {
+    fetch(`http://localhost:5000/api/card/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ state: status }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.success) {
+          handleGetOrders(id);
+        }
+      });
+  };
 
   return (
     <Fragment>
@@ -57,13 +95,14 @@ const Orders = () => {
               <Table striped bordered hover>
                 <thead>
                   <tr>
-                    <th>#</th>
+                    <th>Image</th>
                     <th>title</th>
                     <th>Amount</th>
                     <th>Type</th>
                     <th>Status</th>
-                    <th>Security</th>
-                    <th>Number</th>
+                    <th>Method</th>
+                    <th>Phone</th>
+                    <th>Date</th>
                     <th>Action</th>
                   </tr>
                 </thead>
@@ -72,32 +111,53 @@ const Orders = () => {
                   {orders &&
                     orders.map((item, idx) => (
                       <tr key={idx}>
-                        <td>{idx + 1}</td>
+                        <td style={{ width: "30px" }}>
+                          <img
+                            style={{ width: "25px", height: "25px" }}
+                            src={item?.productId?.images[0]}
+                            alt=""
+                          />
+                        </td>
                         <td>{item?.title}</td>
-                        <td>{item?.amount}</td>
+                        <td>₹ {item?.price}</td>
                         <td>{item?.type}</td>
                         <td>
-                          {(item?.state === "Enable" && (
-                            <span class="badge text-bg-warning">Sold</span>
-                          )) ||
-                            (item?.state === "Disable" && (
-                              <span class="badge text-bg-success text-white">
-                                Sell
-                              </span>
-                            )) ||
-                            (item?.state === "Used" && (
-                              <span class="badge text-bg-info text-white">
+                          <Dropdown>
+                            <Dropdown.Toggle
+                              variant={
+                                (item?.state === "Enable" && "success") ||
+                                item?.state === "danger"
+                              }
+                              id="dropdown-basic"
+                              size="sm"
+                              className="py-1"
+                            >
+                              {item?.state}
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                              <Dropdown.Item
+                                onClick={() =>
+                                  handleUpdateStatus(item?._id, "Enable")
+                                }
+                              >
+                                Enable
+                              </Dropdown.Item>
+                              <Dropdown.Item
+                                onClick={() =>
+                                  handleUpdateStatus(item?._id, "Used")
+                                }
+                              >
                                 Used
-                              </span>
-                            )) ||
-                            (item?.state === "Expired" && (
-                              <span class="badge text-bg-warning text-white">
-                                Expired
-                              </span>
-                            ))}
+                              </Dropdown.Item>
+                            </Dropdown.Menu>
+                          </Dropdown>
                         </td>
-                        <td>{item?.securityCode}</td>
-                        <td>{item?.checkNumber}</td>
+                        <td>
+                          {item?.payment ? item?.payment?.method : "Offline"}
+                        </td>
+                        <td>{item?.address?.mobileNumber}</td>
+                        <td>{moment(item?.createdAt).format("DD/MMM/YYYY")}</td>
                         <td>
                           <Button variant="primary" size="sm">
                             Invoice
@@ -109,6 +169,25 @@ const Orders = () => {
               </Table>
             </div>
           </CardBody>
+          <div className="d-flex justify-content-end justify-items-center p-4">
+            <Button
+              variant="primary"
+              onClick={handlePrevPage}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </Button>
+            <span style={{ margin: "0 10px" }}>
+              Page {currentPage} of {totalPages}
+            </span>
+            <Button
+              variant="primary"
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </Button>
+          </div>
         </Card>
       </Container>
     </Fragment>
